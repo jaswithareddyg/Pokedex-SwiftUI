c576f414ce0a87eaaebaaf02af47280a9508f327
Jas <jaswithareddy@uchicago.edu>
2023-03-08 03:52:14 -0600

    NC0352

---
+++ final-project/final-project/Views/SplashScreen.swift
@@ -0,0 +1,21 @@
+//
+//  SplashScreen.swift
+//  final-project
+//
+//  Created by Jaswitha Reddy G on 3/8/23.
+//
+
+import SwiftUI
+
+struct SplashScreen: View {
+    var body: some View {
+        
+        }
+    }
+}
+
+struct SplashScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        SplashScreen()
+    }
+}
