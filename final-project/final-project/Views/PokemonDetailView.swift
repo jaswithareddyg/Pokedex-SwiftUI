f0c9775fb223a97f02f005779108e1cc98b52021
Jas <jaswithareddy@uchicago.edu>
2023-03-09 02:40:45 -0600

    Info, ScrollView

--- final-project/final-project/Views/PokemonDetailView.swift
+++ final-project/final-project/Views/PokemonDetailView.swift
@@ -8,34 +8,52 @@
 import SwiftUI
 
 struct PokemonDetailView: View {
+    @State private var showPopup = false
     @EnvironmentObject var viewModel: ViewModel
     let pokemon: Pokemon
     
     var body: some View {
-        VStack(spacing: 70.0) {
-            VStack {
-                PokemonView(pokemon: pokemon)
-                VStack(spacing: 10) {
-                    Text("**ID**: \(viewModel.pokemonDetails?.id ?? 0)")
-                    Text("**Weight**: \(viewModel.formatHW(value: viewModel.pokemonDetails?.weight ?? 0)) KG")
-                    Text("**Height**: \(viewModel.formatHW(value: viewModel.pokemonDetails?.height ?? 0)) M")
-                    Text("**Base Experience**: \(viewModel.pokemonDetails?.base_experience ?? 0)")
-                    Text("**Order**: \(viewModel.pokemonDetails?.order ?? 0)")
+        ScrollView{
+            VStack(spacing: 70.0) {
+                HStack(alignment: .center) {
+                    Button(action: {
+                        self.showPopup = true
+                    }) {
+                        Image(systemName: "info.circle")
+                            .font(.system(size: 24))
+                            .foregroundColor(.blue)
+                    }
+                    .padding()
                 }
-                .font(.system(size: 20, weight: .regular, design: .monospaced))
+                
+                VStack {
+                    PokemonView(pokemon: pokemon)
+                    VStack(spacing: 10) {
+                        Text("**ID**: \(viewModel.pokemonDetails?.id ?? 0)")
+                        Text("**Weight**: \(viewModel.formatHW(value: viewModel.pokemonDetails?.weight ?? 0)) KG")
+                        Text("**Height**: \(viewModel.formatHW(value: viewModel.pokemonDetails?.height ?? 0)) M")
+                        Text("**Base Experience**: \(viewModel.pokemonDetails?.base_experience ?? 0)")
+                        Text("**Order**: \(viewModel.pokemonDetails?.order ?? 0)")
+                    }
+                    .font(.system(size: 20, weight: .regular, design: .monospaced))
+                }
+                .padding(.horizontal)
+                .onAppear{
+                    viewModel.getDetails(pokemon: pokemon)
+                }
+                
+                NavigationLink(destination: View3D()) {
+                    Text("View in 3D")
+                        .font(.title3)
+                        .foregroundColor(.black)
+                        .padding()
+                        .background(Color(hue: 1.0, saturation: 0.0, brightness: 0.949))
+                        .cornerRadius(30)
+                        .shadow(color: .gray, radius: 2, x: 3, y: 3)
+                }
             }
-            .onAppear{
-                viewModel.getDetails(pokemon: pokemon)
-            }
-            
-            NavigationLink(destination: View3D()) {
-                                Text("View in 3D")
-                    .font(.title3)
-                                    .foregroundColor(.black)
-                                    .padding()
-                                    .background(Color(hue: 1.0, saturation: 0.0, brightness: 0.949))
-                                    .cornerRadius(30)
-                                    .shadow(color: .gray, radius: 2, x: 3, y: 3)
+            .sheet(isPresented: $showPopup) {
+                        PopupView()
             }
         }
     }
---
+++ final-project/final-project/Views/PopupView.swift
@@ -0,0 +1,48 @@
+//
+//  PopupView.swift
+//  final-project
+//
+//  Created by Jaswitha Reddy G on 3/9/23.
+//
+
+import SwiftUI
+
+struct PopupView: View {
+    @Environment(\.dismiss) var dismiss
+    
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .leading, spacing: 16.0) {
+                HStack {
+                    Spacer()
+                    Button("Cancel") {
+                        dismiss()
+                    }
+                    .font(.system(size: 15, weight: .regular, design: .monospaced))
+                    .padding(20)
+                    .foregroundColor(Color.white)
+                    .background(Color.blue)
+                    .cornerRadius(30)
+                    Spacer()
+                }
+                .padding(.bottom)
+                
+                Text("**ID** is the identifier of the pokémon.")
+                Text("**Height** is the height of the pokémon in meters.")
+                Text("**Weight** is the height of the pokémon in kilograms.")
+                Text("**Base Experience** is the basic experience gained for defeating this Pokémon.")
+                Text("**Order** is the order for sorting. Almost national order, except families are grouped together.")
+            }
+            .font(.system(size: 20, weight: .regular, design: .monospaced))
+            .multilineTextAlignment(.leading)
+            .padding(.horizontal)
+            .padding()
+        }
+    }
+}
+
+struct PopupView_Previews: PreviewProvider {
+    static var previews: some View {
+        PopupView()
+    }
+}
